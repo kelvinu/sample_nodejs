61116ca3ab508ab86df21324306fcf81a877d287
kung <kelvinu@gmail.com>
2013-02-13 13:58:07 +0800

    Added image handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,8 @@
-
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
 var querystring = require("querystring");
+var fs = require("fs");
 
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called.");
 
 	var body = 	'<html>'+
@@ -36,5 +36,24 @@ function upload(response, postData) {
 
 }
 
+function show(response, postData) {
+	
+	console.log("Request handler 'show' was called.");
+	fs.readFile("/tmp/test.png", "binary", function(error, file) {
+							if (error) {
+								response.writeHead(500, {"Content-Type": "text/plain"});
+								response.write(error.text, + "\n");
+								response.end();
+							} else {
+								response.WriteHead(200, {"Content-Type": "image/png"});
+								response.write(file, "binary");
+								response.end();
+							}
+	});
+	
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
+
